b6fa9680552123197ccbfe425c2f49ecf42ec9d8
evanCoakley <evan.coakley1@gmail.com>
2024-06-07 01:48:08 -0500

    initial commit after moving project to Maven build

---
+++ src/main/java/org/coakley/ui/ChatClientUI.java
@@ -0,0 +1,93 @@
+package org.coakley.ui;
+
+import org.coakley.client.ChatClient;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ChatClientUI extends JFrame {
+    private JTextArea messageArea;
+    private JTextField textField;
+    private JButton exitButton;
+    private ChatClient client;
+
+    public ChatClientUI(){
+        super("Chat App");
+        setSize(400, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        Color backgroundColor = new Color(60,60,60);
+        Color buttonColor = new Color(250,75,75);
+        Color textColor = new Color(250,250,250);
+        Font textFont = new Font("Roboto Mono", Font.PLAIN, 14);
+        Font buttonFont = new Font("Roboto Mono", Font.BOLD,12);
+
+        messageArea = new JTextArea();
+        messageArea.setEditable(false);
+        messageArea.setForeground(textColor);
+        messageArea.setBackground(backgroundColor);
+        messageArea.setFont(textFont);
+        JScrollPane scrollPane = new JScrollPane(messageArea);
+        add(scrollPane, BorderLayout.CENTER);
+
+        String name = JOptionPane.showInputDialog(this, "Enter you name: ", "Name Entry", JOptionPane.PLAIN_MESSAGE);
+        this.setTitle("Chat App - " + name);
+
+        textField = new JTextField();
+        textField.setFont(textFont);
+        textField.setForeground(textColor);
+        textField.setBackground(backgroundColor);
+        textField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = "[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "]" + name + ": " + textField.getText();
+                client.sendMessage(message);
+                textField.setText("");
+            }
+        });
+
+        exitButton = new JButton("Exit");
+        exitButton.setForeground(Color.BLACK);
+        exitButton.setBackground(buttonColor);
+        exitButton.setFont(buttonFont);
+        exitButton.addActionListener(e -> {
+            String departureMessage = name + " has left the chat.";
+            client.sendMessage(departureMessage);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie){
+                Thread.currentThread().interrupt();
+            }
+            System.exit(0);
+        });
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.setBackground(backgroundColor);
+        bottomPanel.add(textField, BorderLayout.CENTER);
+        bottomPanel.add(exitButton, BorderLayout.EAST);
+        add(bottomPanel, BorderLayout.SOUTH);
+        try {
+            this.client = new ChatClient("127.0.0.1",2000, this::onMessageReceived);
+            client.startClient();
+        } catch (IOException e){
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this,"Error connecting the server", "Connection Error", JOptionPane.ERROR_MESSAGE);
+            System.exit(1);
+        }
+
+    }
+    private void onMessageReceived(String message){
+        SwingUtilities.invokeLater(() -> messageArea.append(message + "\n"));
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new ChatClientUI().setVisible(true);
+        });
+    }
+}
